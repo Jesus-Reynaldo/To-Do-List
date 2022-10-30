ecd51da7128c5724e304cb948851e27dfc7c0f79
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2022-10-30 09:39:53 -0400

    Componentes de TO-DO LIST

---
+++ src/TodoCounter.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const estilos = {
+  color: "red",
+  backgroundColor: "yellow",
+};
+export const TodoCounter = () => {
+  return <h2 style={estilos}>Has completado 2 de 3 tares</h2>;
+};
